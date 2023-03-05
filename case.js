5c90a6f3705921c1e7570f3cbd6082f39875352e
ras1k <rasikabdullah@gmail.com>
2023-03-05 13:04:46 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,9 @@
+const userName = 'GreenDay';
+const input = 'greenday';
+
+if (userName.toLowerCase() == input.toLowerCase()){
+    console.log('valid user');
+}
+else {
+    console.log('invalid user');
+}
---
+++ search.js
@@ -0,0 +1,1 @@
+const lyrics = 'the less i know, the better';
